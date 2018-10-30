8f0eedd6f2320bc54ba7bb1706634fbee04bcc3e
Christoph Purrer <chpurrer@fb.com>
2018-10-30 10:34:19 -0700

    Fix renderer.js issue

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-
-ipcRenderer.on('DEEP_LINK', function (event, data) { console.log(data.msg) });
+const {ipcRenderer} = require('electron');
+ipcRenderer.on('DEEP_LINK', function (event, data) { 
+    console.log(data) 
+});
